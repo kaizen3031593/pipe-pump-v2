7783561066a8a2e9e7a3391617459c5577f26713
kaizen3031593 <zen.conroy@gmail.com>
2021-08-19 16:23:47 -0400

    set flag for bootstrapping and add aws act

--- private/configuration-example.ts
+++ private/configuration-example.ts
@@ -2,4 +2,4 @@ export const githubUser:string = 'githubuser';
 export const githubRepo:string = 'githubrepo';
 export const githubBranch:string = 'githubbranch';
 export const codeStarConnectionArn:string = 'arn';
-export const awsAccount:string = '00000000';
+export const awsAccount:string = '000000000000';
