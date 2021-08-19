91cea5191734f1c920d190f0a1016231277146a8
kaizen3031593 <zen.conroy@gmail.com>
2021-08-19 16:38:20 -0400

    add configuration file

--- private/configuration-example.ts
+++
@@ -1,6 +0,0 @@
-export const githubUser:string = 'githubuser';
-export const githubRepo:string = 'githubrepo';
-export const githubBranch:string = 'githubbranch';
-export const codeStarConnectionArn:string = 'arn';
-export const awsAccount:string = '000000000000';
-export const awsRegion:string = 'us-west-1';
---
+++ private/configuration.ts
@@ -0,0 +1,6 @@
+export const githubUser:string = 'kaizen3031593';
+export const githubRepo:string = 'pipe-pump-v2';
+export const githubBranch:string = 'main';
+export const codeStarConnectionArn:string = 'arn:aws:codestar-connections:us-east-1:489318732371:connection/43e5f397-b6b4-4092-b7ac-499be741f978';
+export const awsAccount:string = '489318732371';
+export const awsRegion:string = 'us-west-1';
