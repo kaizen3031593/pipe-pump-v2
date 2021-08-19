c36215552927aa34c590055dff06b9e175569fbe
kaizen3031593 <zen.conroy@gmail.com>
2021-08-19 12:20:09 -0400

    rename files

--- bin/pipeline.ts
+++ bin/pipeline.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { PipelinesStack } from '../lib/pipelines-stack';
+import { CanaryStack } from '../lib/canary-stack';
 
 const app = new cdk.App();
-new PipelinesStack(app, 'PipelinesStack', {
+new CanaryStack(app, 'PipelinesStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/canary-stack.ts
+++ lib/canary-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 
-export class PipelinesStack extends cdk.Stack {
+export class CanaryStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
